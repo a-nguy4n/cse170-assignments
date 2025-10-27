112b6cda507b1ea9e9d5e807bac8bd2d4138d857
a-nguy4n <1512alli@gmail.com>
2025-10-27 13:18:21 -0700

    Added Lab 7 Starter Code

---
+++ lab-Seven/labSeven.js
@@ -0,0 +1,8 @@
+
+// Starter JS code from the Google Drive Link provided in instructions
+// You will need to write the Javascript for the carousel button functionality. We  provide only some example images. 
+let imageURLs = [
+    "https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdXB3azYxNzkxOTM5LXdpa2ltZWRpYS1pbWFnZS1rb3dic3dsMC5qcGc.jpg", 
+    "https://upload.wikimedia.org/wikipedia/commons/5/53/Lake_Kinney_mit_Mount_Whitehorn.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Russell_Falls_2.jpg/540px-Russell_Falls_2.jpg"  
+];
