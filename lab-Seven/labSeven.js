7900cd5007b3ec8d038a0fd2f1268609378a8574
a-nguy4n <1512alli@gmail.com>
2025-11-04 11:40:27 -0800

    Lab 7 Code

--- lab-Seven/labSeven.js
+++ lab-Seven/labSeven.js
@@ -4,5 +4,66 @@
 let imageURLs = [
     "https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdXB3azYxNzkxOTM5LXdpa2ltZWRpYS1pbWFnZS1rb3dic3dsMC5qcGc.jpg", 
     "https://upload.wikimedia.org/wikipedia/commons/5/53/Lake_Kinney_mit_Mount_Whitehorn.jpg",
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Russell_Falls_2.jpg/540px-Russell_Falls_2.jpg"  
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/202506_%E6%B7%B1%E5%A4%9C%E5%9C%A8%E5%86%B7%E6%B0%B4%E5%9D%91%E9%81%8A%E5%AE%A2%E6%9C%8D%E5%8B%99%E7%AB%99%E7%9E%AD%E6%9C%9B%E7%9A%84%E7%A9%8D%E9%9B%A8%E9%9B%B2%E6%89%93%E9%9B%B7_01.jpg/1599px-202506_%E6%B7%B1%E5%A4%9C%E5%9C%A8%E5%86%B7%E6%B0%B4%E5%9D%91%E9%81%8A%E5%AE%A2%E6%9C%8D%E5%8B%99%E7%AB%99%E7%9E%AD%E6%9C%9B%E7%9A%84%E7%A9%8D%E9%9B%A8%E9%9B%B2%E6%89%93%E9%9B%B7_01.jpg?20250608231039"  
 ];
+
+
+function replaceImg(index){
+    const currentImg = document.querySelector(".carousel");
+    const replacementImg = document.createElement('img');
+    replacementImg.className = "carousel";
+    replacementImg.src = imageURLs[index];
+    replacementImg.alt = "Image " + (index + 1);
+    replacementImg.id = String(index);
+
+    console.log("Current id is:", currentImg.id);
+    currentImg.replaceWith(replacementImg);
+}
+
+
+// get image id 
+// check what number image id is 
+// if img id is 1 or 2 decrement 
+// if img id is 0 change to 2
+
+function prev(){
+    const previousButton = document.querySelector('.prev');
+    previousButton.addEventListener('click', () => {
+        const currentImage = document.querySelector(".carousel");
+        const currentImageIndex = parseInt(currentImage.id);
+        const previousImageIndex = (currentImageIndex - 1 +  imageURLs.length) % imageURLs.length;
+        
+        console.log("previous button clicked");
+        replaceImg(previousImageIndex);
+    });
+};
+
+
+// get image id 
+// check what number image id is 
+// if img id is 0 or 1 increment to next 
+// if img id is 2 change to 0 
+function next(){
+    const nextButton = document.querySelector('.next');
+   
+    nextButton.addEventListener('click', () => {
+        const currentImage = document.querySelector(".carousel");
+        const currentImageIndex = parseInt(currentImage.id);
+        const nextImageIndex = (currentImageIndex + 1) % imageURLs.length;
+
+        console.log("next button clicked");
+        replaceImg(nextImageIndex);
+    });
+};  
+
+
+window.addEventListener("DOMContentLoaded", () => {
+
+    const starterImg = document.querySelector(".carousel");
+    if(starterImg && starterImg.id == ''){
+        starterImg.id = "0";
+    }
+
+    prev();
+    next();
+});
--- lab-Six/labSix.js
+++ lab-Six/labSix.js
@@ -1,33 +1,37 @@
 
-// for Trick One 
-const trickOne_Button = document.querySelector('#trick-one button');
-const trickOne_Text = document.querySelector('#trick-one p');
+document.addEventListener('DOMContentLoaded', () => {
+    
+    // for Trick One 
+    const trickOne_Button = document.querySelector('#trick-one button');
+    const trickOne_Text = document.querySelector('#trick-one p');
 
-trickOne_Button.addEventListener('click', () => {
-    trickOne_Text.innerHTML = "🎩 Ta-Daaaa! Ala-Kazam! The Waverly Wand does Magical Wanders 💫";
+    trickOne_Button.addEventListener('click', () => {
+        trickOne_Text.textContent = "🎩 Ta-Daaaa! Ala-Kazam! The Waverly Wand does Magical Wanders 💫";
+    });
+
+    // for Trick Two 
+    const trickTwo_Button = document.querySelector('#trick-two button');
+    const trickTwo_Text = document.querySelector('#trick-two p');
+
+    trickTwo_Button.addEventListener('click', () => {
+        trickTwo_Text.style.color = "#42BD00";
+    });
+
+    // for Trick Three 
+    const trickThree_Button = document.querySelector('#trick-three button');
+    const trickThree_Text = document.querySelector('#trick-three p');
+    const trickThree_Section = document.querySelector('#trick-three');
+    const trickThree_Image = document.createElement('img');
+    trickThree_Image.src = "/lab-Six/trickThree.gif";
+    trickThree_Image.alt = "Magic Bunny GIF";
+
+    trickThree_Button.addEventListener('click', () => {
+        if(trickThree_Text){
+            trickThree_Text.remove();
+        }
+        trickThree_Section.insertBefore(trickThree_Image, trickThree_Button);
+    })
 });
 
 
-// for Trick Two 
-const trickTwo_Button = document.querySelector('#trick-two button');
-const trickTwo_Text = document.querySelector('#trick-two p');
 
-trickTwo_Button.addEventListener('click', () => {
-    trickTwo_Text.style.color = "#42BD00";
-});
-
-
-// for Trick Three 
-const trickThree_Button = document.querySelector('#trick-three button');
-const trickThree_Text = document.querySelector('#trick-three p');
-const trickThree_Section = document.querySelector('#trick-three');
-const trickThree_Image = document.createElement('img');
-trickThree_Image.src = "/lab-Six/trickThree.gif";
-trickThree_Image.alt = "Magic Bunny GIF";
-
-trickThree_Button.addEventListener('click', () => {
-    if(trickThree_Text){
-         trickThree_Text.remove();
-    }
-    trickThree_Section.insertBefore(trickThree_Image, trickThree_Button);
-});
